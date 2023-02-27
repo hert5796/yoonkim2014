6ce88d2ebbcfd451b4d5dc10745517977a5c1265
Aaron Omotosho <hert5796@gmail.com>
2023-02-27 00:37:20 +0000

    changed to maxlen 200

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ CORS(app)
 
 model = keras.models.load_model('model.h5')
 word_index = keras.datasets.imdb.get_word_index()
+max_len = 200
 
 
 def encode_text(text):
     tokens = keras.preprocessing.text.text_to_word_sequence(text)
     tokens = [word_index[word] if word in word_index else 0 for word in tokens]
-    return keras.preprocessing.sequence.pad_sequences([tokens], maxlen=200)
+    return keras.preprocessing.sequence.pad_sequences([tokens], maxlen=max_len)
 
 
 @app.route('/', methods=['POST'])
